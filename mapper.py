f3edb6926c61ed78ef6ed9fc24aaaa0347ed945e
izabellasvavolya <168412230+izabellasvavolya@users.noreply.github.com>
2025-11-30 02:46:46 +0200

    mapper ex2

--- mapper.py
+++ mapper.py
@@ -1,6 +1,19 @@
-import sys, string
-for line in sys.stdin:
-    for w in line.strip().split():
-        w = w.strip(string.punctuation).lower()
-        if w:
-            print(f"{w}\t1")
+import sys
+
+def read_input(file):
+    for line in file:
+        yield line.split()
+
+def main(separator='\t'):
+    data = read_input(sys.stdin)
+    for words in data:
+        for word in words:
+    
+            word = word.lower()
+            word = word.rstrip(".,")
+            
+            if word:
+                print("%s%s%d" % (word, separator, 1))
+
+if __name__ == "__main__":
+    main()
